f80e46ed631ef125681909ee07af85b6482e2c05
noni-was-taken <ninocalunod.info@gmail.com>
2025-04-16 02:59:24 +0800

    fetch api works now, icons also added but not implemented yet

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-let weatherData = [];
-const container = document.querySelector('.container');
-const accessCode = 'c68dde0bc16acf498555406d31fedf76';
+const APIkey = 'XRZU7BWG5WUQZQC4WRWNLD9YW';
+const userLocationInput = document.getElementById(`inputLocation`);
+
 
 async function getData(){
-    let location = document.getElementById('inputLocation').value;
     try{
-        response = await fetch(`http://api.weatherstack.com/current?access_key=${accessCode}&query=${location}`)
-        if(!response.ok){
-            throw new Error("nt tried");
+        let tempInput = document.getElementById(`inputLocation`).value;
+        data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${tempInput}?unitGroup=us&key=${APIkey}`)
+        if(!data.ok){
+            throw 'we failed';
         }
-        const locationData = await response.json();
-        console.log(locationData);
-
-        return locationData;
+        let receivedData = data.json();
+        return receivedData;
     }
     catch(error){
-        console.error(error);
+        console.log(error);
     }
+};
+
+userLocationInput.addEventListener(`keydown`, function(event){
+    if(event.key == 'Enter'){
+        fetchLocationData();
+    }
+});
+
+async function fetchLocationData(){
+    let locationData = await getData();
+    console.log(locationData);
+    displayFeatured(locationData);
+    
+} 
+
+function displayFeatured(data){
+    document.getElementById('cityName').textContent = data.resolvedAddress;
+    document.getElementById('timeInformation').textContent = `${data.timezone} - UTC${data.tzoffset>=1 ? '+'+data.tzoffset : data.tzoffset}`
+    document.getElementById('weatherStatus').textContent = data.days[0].conditions;
+    document.getElementById('temperature').textContent = `${Math.ceil(data.days[0].temp)}°`;
 }
-
-
-
