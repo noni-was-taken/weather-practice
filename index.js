1846cf3d05ac87399465680eaba46d9ef26f8c0d
noni-was-taken <ninocalunod.info@gmail.com>
2025-04-17 18:55:38 +0800

    bout to be archived, im tired of this project for now

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const APIkey = 'XRZU7BWG5WUQZQC4WRWNLD9YW';
 const userLocationInput = document.getElementById(`inputLocation`);
 
+document.addEventListener("DOMContentLoaded", async (event) => { 
+    let locationData = await getData("Japan");
+    console.log(locationData);
+    displayFeatured(locationData);
+});
 
-async function getData(){
+async function getData(inputLocation){
     try{
-        let tempInput = document.getElementById(`inputLocation`).value;
-        data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${tempInput}?unitGroup=us&key=${APIkey}`)
+        data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputLocation}?unitGroup=us&key=${APIkey}`)
         if(!data.ok){
-            throw 'we failed';
+            throw 'Failed to fetch data.';
         }
         let receivedData = data.json();
         return receivedData;
@@ -24,10 +28,10 @@ userLocationInput.addEventListener(`keydown`, function(event){
 });
 
 async function fetchLocationData(){
-    let locationData = await getData();
+    let locationInput = document.getElementById('inputLocation').value;
+    let locationData = await getData(locationInput);
     console.log(locationData);
     displayFeatured(locationData);
-    
 } 
 
 function displayFeatured(data){
@@ -35,4 +39,6 @@ function displayFeatured(data){
     document.getElementById('timeInformation').textContent = `${data.timezone} - UTC${data.tzoffset>=1 ? '+'+data.tzoffset : data.tzoffset}`
     document.getElementById('weatherStatus').textContent = data.days[0].conditions;
     document.getElementById('temperature').textContent = `${Math.ceil(data.days[0].temp)}°`;
+
+    document.getElementById('weatherIcon').setAttribute('src', `/weatherIcons/${data.days[0].icon}.png`)
 }
